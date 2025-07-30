58e3528833aed8f6d9842efee7c4c19850a97553
Zhunina Lena <zhunina.lena@yandex.ru>
2025-07-30 23:51:59 +0300

    add ru.js

--- src/add.js
+++ src/add.js
@@ -17,3 +17,30 @@ const validateURL = async (url, parsedLinks) => {
     .required();
   return schema.validate(url);
 };
+
+const watchedState = onChange(state, (path, value, previousValue) => {
+  switch (path) {
+    case 'loadingProcess':
+    case 'error':
+      handleFormAccessibility(elements, watchedState);
+      handleErrors(state.error, state);
+      renderFeedback(elements, state, i18n);
+      break;
+    case 'parsedFeeds':
+      renderFeed(elements, state, i18n);
+      break;
+    case 'parsedPosts':
+    case 'uiState.viewedLinks':
+      renderPosts(elements, state, i18n);
+      break;
+    case 'uiState.clickedPostLink':
+      renderModals(elements, state);
+      break;
+    case 'lng':
+      i18n.changeLanguage(value);
+      renderLanguage(elements, value, previousValue, i18n);
+      break;
+    default:
+      break;
+  }
+});
