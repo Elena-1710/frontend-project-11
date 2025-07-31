ca0377d523388f392a0087160d9a72d474665c54
Zhunina Lena <zhunina.lena@yandex.ru>
2025-08-01 00:34:18 +0300

    mod teg a

--- eslint.config.js
+++ eslint.config.js
@@ -5,5 +5,8 @@ import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig(
   stylistic.configs.recommended, [
-    { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'], languageOptions: { globals: globals.browser } },
+    { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'], languageOptions: { globals: globals.browser }, rules: {
+        'jsx-a11y/anchor-is-valid': 'off',
+        'jsx-a11y/click-events-have-key-events': 'off'
+      } },
   ])
