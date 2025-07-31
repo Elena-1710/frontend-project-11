562e9b9255a73fe37b4fc584ebab19dfa2e23cb1
Zhunina Lena <zhunina.lena@yandex.ru>
2025-08-01 00:37:35 +0300

    mog eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,8 +5,7 @@ import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig(
   stylistic.configs.recommended, [
-    { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'], languageOptions: { globals: globals.browser }, rules: {
-        'jsx-a11y/anchor-is-valid': 'off',
-        'jsx-a11y/click-events-have-key-events': 'off'
-      } },
+    { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'], languageOptions: { globals: globals.browser }, rules:
+        { 'jsx-a11y/anchor-is-valid': 'off', 'jsx-a11y/click-events-have-key-events': 'off',
+        } },
   ])
